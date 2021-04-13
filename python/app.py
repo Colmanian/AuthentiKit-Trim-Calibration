45f085fd3835a536f0fb9629fda318586b6c6bff
Ian Colman <colmanianuk@gmail.com>
2021-04-13 23:13:51 +0100

    can interface with x64 vJoy DLL

--- python/app.py
+++ python/app.py
@@ -1,7 +1,14 @@
 import pygame
 import keyboard
+import pyvjoy
+from time import sleep
 
-# Initialize the joysticks.
+# Author: Ian Colman
+# License: Creative Commons BY NC ND 4.0
+# Credits:  https://github.com/tidzo/pyvjoy for the vJoy DLL wrapper 
+#           https://sourceforge.net/projects/vjoystick/ for the vJoy DLL
+
+# Initialize the real joysticks
 pygame.init()
 pygame.joystick.init()
 joystick_count = pygame.joystick.get_count()
@@ -9,14 +16,17 @@ for i in range(joystick_count):
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
 
+# Initialise the virtual joysticks
+j = pyvjoy.VJoyDevice(1)
+
 done = False
 while not done:
     
     if keyboard.is_pressed('esc'):
             done = True 
 
-    key_map = {10:'NUM 1', 11:'NUM 7'}
-    multiplier_map = {10:10, 11:10}
+    button_map = {10:1, 11:2}
+    multiplier_map = {10:3, 11:3}
 
     for event in pygame.event.get():
         if event.type == pygame.JOYBUTTONDOWN:
@@ -26,4 +36,7 @@ while not done:
             button = event.button
             print("{}".format(name), "Button {}".format(button))
             for n in range(0, multiplier_map[button]):
-                keyboard.send(key_map[event.button])
+                j.set_button(button_map[event.button],1)
+                sleep(0.05)
+                j.set_button(button_map[event.button],0)
+                sleep(0.05)
