50b6d205dac31d1f40244667f43f8f4bc202063b
Ian Colman <colmanianuk@gmail.com>
2021-04-14 15:56:38 +0100

    Can trim at maxed out speed now

--- python/app.py
+++ python/app.py
@@ -37,24 +37,59 @@ for i in range(joystick_count):
 # Get virtual joystick info
 j = pyvjoy.VJoyDevice(1)
 
+
+# Hard code mappings and multipliers (for proof of concept)
+button_map = {10:1, 11:2}
+multiplier_map = {10:4, 11:4}
+
+# Timing information
+ticks = 0
+HOLD_ON_THRESHOLD_MS = 100
+active_button = None # (Only supports one button active at a time currently)
+
+# Main Loop
 done = False
 while not done:
-    
+
+    sleep(0.001)
+    ticks += 1
+
     if keyboard.is_pressed('esc'):
             done = True 
 
-    button_map = {10:1, 11:2}
-    multiplier_map = {10:4, 11:4}
+    # for event in pygame.event.get():
+    #     if event.type == pygame.JOYBUTTONDOWN:
+    #         #joystick = pygame.joystick.Joystick(event.joy)
+    #         #joystick.init()
+    #         #name = joystick.get_name()
+    #         button = event.button
+    #         #print("{}".format(name), "Button {}".format(button))
+    #         for n in range(0, multiplier_map[button]):
+    #             j.set_button(button_map[event.button],1)
+    #             sleep(0.02)
+    #             j.set_button(button_map[event.button],0)
+    #             sleep(0.02)
 
     for event in pygame.event.get():
         if event.type == pygame.JOYBUTTONDOWN:
-            #joystick = pygame.joystick.Joystick(event.joy)
-            #joystick.init()
-            #name = joystick.get_name()
+            joystick = pygame.joystick.Joystick(event.joy).get_name()
             button = event.button
-            #print("{}".format(name), "Button {}".format(button))
-            for n in range(0, multiplier_map[button]):
-                j.set_button(button_map[event.button],1)
-                sleep(0.02)
-                j.set_button(button_map[event.button],0)
-                sleep(0.02)
+
+            if (joystick == "BU0836 Interface"):
+
+                # If we're still turning the active trim wheel, reset the timer
+                if button == active_button:
+                    ticks = 0
+
+                # If we're starting to turn a trim wheel, then turn out the output 
+                elif active_button == None and (button in button_map):
+                    ticks = 0
+                    active_button = button
+                    j.set_button(button_map[active_button],1)
+                
+                # Else ignore all other events while we're dealing with this trim turn
+    
+    # If we're outputting but haven't seen an movement for the threshold time, then turn it off
+    if (active_button != None) and (ticks > HOLD_ON_THRESHOLD_MS):
+            j.set_button(button_map[active_button],0)
+            active_button = None
