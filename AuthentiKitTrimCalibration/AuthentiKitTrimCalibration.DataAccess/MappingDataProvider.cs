49820b669e5eb77b0453706d0251bec272d565af
Ian Colman <colmanianuk@gmail.com>
2021-04-26 19:59:36 +0100

    added dummy mapping thread

--- AuthentiKitTrimCalibration/AuthentiKitTrimCalibration.DataAccess/MappingDataProvider.cs
+++ AuthentiKitTrimCalibration/AuthentiKitTrimCalibration.DataAccess/MappingDataProvider.cs
@@ -2,12 +2,52 @@
 using MappingManager.Common.Model;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 using vJoyInterfaceWrap;
 
 namespace AuthentiKitTrimCalibration.DataAccess
 {
     public class MappingDataProvider : IMappingDataProvider
     {
+        private Thread _mappingThread;
+
+        public static void MappingProcess()
+        {
+            try
+            {
+
+                Debug.WriteLine("I've started mapping");
+                Debug.WriteLine("*** ATTEMPTING TO USE VJOY *** ");
+                var vJoystick = new vJoy();
+                if (!vJoystick.vJoyEnabled())
+                {
+                    Debug.WriteLine("vJoy driver not enabled: Failed Getting vJoy attributes.\n");
+                }
+                else
+                {
+                    Debug.WriteLine("Vendor: {0}\nProduct :{1}\nVersion Number:{2}\n", vJoystick.GetvJoyManufacturerString(), vJoystick.GetvJoyProductString(), vJoystick.GetvJoySerialNumberString());
+                }
+
+                // Output something
+                int i = 0;
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    Debug.WriteLine(i);
+                    i++;
+                }
+            }
+            catch (ThreadAbortException e)
+            {
+                Debug.WriteLine("Thread Abort Exception: ", e);
+            }
+            finally
+            {
+                Debug.WriteLine("Something else went wrong");
+            }
+
+        }
+
         public IEnumerable<Mapping> LoadMappings()
         {
             return new List<Mapping>
@@ -55,21 +95,16 @@ namespace AuthentiKitTrimCalibration.DataAccess
         public void ApplyMapping(Mapping mapping)
         {
             Debug.WriteLine($"APPLY IS UNIMPLEMTNED: {mapping.Name}");
+            if(_mappingThread == null)
+            {
+                var mappingThreadRef = new ThreadStart(MappingProcess);
+                _mappingThread = new Thread(mappingThreadRef);
+                _mappingThread.Start();
+            }
         }
 
         public IEnumerable<Device> LoadDevices()
         {
-            Debug.WriteLine("*** ATTEMPTING TO USE VJOY *** ");
-            var vJoystick = new vJoy();
-            if (!vJoystick.vJoyEnabled())
-            {
-                Debug.WriteLine("vJoy driver not enabled: Failed Getting vJoy attributes.\n");
-            }
-            else
-            {
-                Debug.WriteLine("Vendor: {0}\nProduct :{1}\nVersion Number:{2}\n", vJoystick.GetvJoyManufacturerString(), vJoystick.GetvJoyProductString(), vJoystick.GetvJoySerialNumberString());
-            }
-
             Debug.WriteLine("LOAD DEVICES IS UNIMPLEMTNED");
             return new List<Device>
             {
