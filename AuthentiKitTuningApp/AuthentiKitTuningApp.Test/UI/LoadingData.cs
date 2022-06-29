8874059cfc84f9f60a148d1f0f9c25c5968c69f6
Ian Colman <colmanianuk@gmail.com>
2022-06-29 11:44:53 +0100

    Added Test - Start with non existing save file

--- AuthentiKitTuningApp/AuthentiKitTuningApp.Test/UI/LoadingData.cs
+++ AuthentiKitTuningApp/AuthentiKitTuningApp.Test/UI/LoadingData.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
+using System.Diagnostics;
 
 namespace AuthentiKitTuningApp.Test.UI
 {
@@ -34,6 +36,50 @@ namespace AuthentiKitTuningApp.Test.UI
                 session.FindElementByName("Delete").Click();
             }
 
+            Assert.AreEqual(false, AppHasThrownException());
+            Assert.AreEqual(true, AppIsRunning());
+            AuthenitKitTuningAppSession.TearDown();
+        }
+
+
+
+        [TestMethod]
+        public void Start_With_Non_Exsisting_Save_File()
+        {
+            const string TEMP_FILE_NAME = "TempTestFile.xml";
+            AuthenitKitTuningAppSession.Setup();
+            var session = AuthenitKitTuningAppSession.session;
+            if (session != null)
+            {
+                // Create a new save file
+                session.FindElementByName("menuStrip").SendKeys("%p");
+                session.FindElementByName("Clear All").Click();
+                session.FindElementByName("File").Click();
+                session.FindElementByName("Save As").Click();
+                Actions actions = new Actions(session);
+                actions.SendKeys(TEMP_FILE_NAME).Perform();
+                session.FindElementByName("Save").Click();
+                actions = new Actions(session);
+                actions.SendKeys("y{enter}").Perform();
+
+                // Delete the save file from the filesystem
+                session.FindElementByName("File").Click();
+                session.FindElementByName("Load").Click();
+                session.FindElementByName(TEMP_FILE_NAME).Click();
+                actions = new Actions(session);
+                actions.ContextClick().SendKeys("d").Perform();
+                session.FindElementByName("Cancel").Click();
+            }
+            else
+            {
+                Assert.IsNotNull(session);
+            }
+
+            // Restart the app
+            AuthenitKitTuningAppSession.TearDown();
+            AuthenitKitTuningAppSession.Setup();
+
+            // Verify all is well
             Assert.AreEqual(false, AppHasThrownException());
             Assert.AreEqual(true, AppIsRunning());
             AuthenitKitTuningAppSession.TearDown();
